35154113242a43701a27ef11659801f825ecfb0e
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-03 17:25:45 +0900

    조작을 하기 위한 플레이어 파일 추가

--- game_framework.py
+++ game_framework.py
@@ -9,13 +9,15 @@ screen_height = 864
 
 
 
-def ingame():
+def ingame(game_manager):
     global running
     running = True
 
     global frame_time
     frame_time = 0.0
     current_time = time.time()
+    game_manager.update()
+    game_manager.render()
     while running:
         frame_time = time.time() - current_time
         current_time += frame_time
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pico2d
 import game_framework
+import game_manager
 
 
 pico2d.open_canvas(game_framework.screen_width, game_framework.screen_height)
-game_framework.ingame()
+game_framework.ingame(game_manager)
 pico2d.close_canvas()
 
 # 2022180021 양현빈
