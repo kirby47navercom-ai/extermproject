dd28445cd7ad6c6dac841b1199b2bf44adb98861
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-03 13:48:45 +0900

    창을 띄우고 델타타임을 만들어줌 ingame함수 완성

--- game_framework.py
+++ game_framework.py
@@ -6,5 +6,19 @@ root = tk.Tk()
 screen_width = 1536
 screen_height = 864
 
+
+
+
 def ingame():
+    global running
+    running = True
+
+    global frame_time
+    frame_time = 0.0
+    current_time = time.time()
+    while running:
+        frame_time = time.time() - current_time
+        current_time += frame_time
+
+
     pass
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import game_framework
 
 
 pico2d.open_canvas(game_framework.screen_width, game_framework.screen_height)
-
+game_framework.ingame()
 pico2d.close_canvas()
 
 # 2022180021 양현빈
