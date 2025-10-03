901b825544fbd596631dc8bdf4270338398218b3
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-03 13:35:58 +0900

    메인 파일 만듬

--- draw.py
+++ draw.py
@@ -2,7 +2,7 @@ from pico2d import *
 import os
 from dtd import QDollarRecognizer, Point
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 800
+SCREEN_WIDTH, SCREEN_HEIGHT = 1536, 864
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
