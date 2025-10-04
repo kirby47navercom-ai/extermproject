a76b067a66fb2caeefe4c6a88596f3b7daa1728f
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-05 01:14:42 +0900

    마우스 변경 완료

--- game_manager.py
+++ game_manager.py
@@ -1,27 +1,28 @@
 from pico2d import *
 import ramona
 import background
+import mouse_image
 
 def init():
-    global player
-    global stage_background
+    global player,stage_background,mouse
     player = ramona.Ramona()
     stage_background = background.Background('1')
+    mouse = mouse_image.Mouse()
 
     pass
 
 def update(frame_time):
-    global player
-    global stage_background
+    global player, stage_background
     stage_background.update(frame_time)
     player.update(frame_time)
+    mouse.update(frame_time)
     pass
 
 def render():
-    global player
-    global stage_background
+    global player, stage_background
     clear_canvas()
     stage_background.draw()
     player.draw()
+    mouse.draw()
     update_canvas()
     pass
--- mouse_image.py
+++ mouse_image.py
@@ -1,9 +1,9 @@
 from pico2d import *
 
-
+width, height = 128, 128
 
 class Mouse:
-    def __init__(self, stage):
+    def __init__(self):
         self.normal = load_image('Mouse\\1.png')
         self.paint = load_image('Mouse\\2.png')
         self.x=0
@@ -11,10 +11,13 @@ class Mouse:
         pass
 
     def update(self, frame_time):
-        if event.type == SDL_MOUSEMOTION:
-            self.x, self.y = event.x, get_canvas_height() - 1 - event.y
+        events = get_events()
+        for event in events:
+            if event.type == SDL_MOUSEMOTION:
+                self.x, self.y = event.x, get_canvas_height() - 1 - event.y
         pass
 
     def draw(self):
-
+        self.normal.clip_draw(0, 0, 128, 128, self.x+30, self.y-30,
+                                     width * 0.5, height * 0.5)
         pass
