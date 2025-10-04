e7df6f99ad37f885e3754e119ee04c81c164518d
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-05 01:07:33 +0900

    마우스 사진 가져옴

--- mouse_image.py
+++ mouse_image.py
@@ -4,12 +4,17 @@ from pico2d import *
 
 class Mouse:
     def __init__(self, stage):
-
+        self.normal = load_image('Mouse\\1.png')
+        self.paint = load_image('Mouse\\2.png')
+        self.x=0
+        self.y=0
         pass
 
     def update(self, frame_time):
-
+        if event.type == SDL_MOUSEMOTION:
+            self.x, self.y = event.x, get_canvas_height() - 1 - event.y
         pass
 
     def draw(self):
+
         pass
