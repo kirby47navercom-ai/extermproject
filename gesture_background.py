2e8786bee2a4b13ce2775a7d6945a3b2514108a0
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-06 01:48:59 +0900

    캔버스 사이즈 한꺼번에 하기 위해 만듬

---
+++ gesture_background.py
@@ -0,0 +1,18 @@
+from pico2d import *
+
+canvaswidth = 1280
+canvasheight = 720
+
+class GestureBackground:
+    def __init__(self):
+        self.image = load_image('Canvas\\1.png')
+        self.x = 400
+        self.y = 300
+        pass
+
+    def update(self, frame_time, events):
+        pass
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        pass
