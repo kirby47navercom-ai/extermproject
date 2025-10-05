4fd4f794e2cc3aec4533671290635b3c3231e74e
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-06 03:19:21 +0900

    f키로 열기닫기 함

--- gesture_background.py
+++ gesture_background.py
@@ -3,7 +3,7 @@ from canvas_size import *
 
 check_image_width = 825
 check_image_height = 216
-
+SIZE=20
 
 
 class GestureBackground:
@@ -14,20 +14,37 @@ class GestureBackground:
         self.check_image_y = canvasheight-(check_image_height*0.2)
         self.canvas_image_x=canvaswidth//2
         self.canvas_image_y=canvasheight+canvasheight//2
-
+        self.f_pressed = True
+        self.go=False
         pass
 
     def update(self, frame_time, events):
-        for event in events:
 
+        self.handle_event(events)
+
+        if self.f_pressed==False:
             if self.canvas_image_y>canvasheight//2:
-                self.check_image_y-=10
-                self.canvas_image_y-=10
+                self.check_image_y-=SIZE
+                self.canvas_image_y-=SIZE
+            else:   self.go=True
+        else:
+            if self.canvas_image_y<canvasheight+canvasheight//2:
+                self.check_image_y+=SIZE
+                self.canvas_image_y+=SIZE
+                self.go=False
         pass
 
 
-    def handle_event(self, event):
-        pass
+    def handle_event(self, events):
+        for event in events:
+            if event.type == SDL_KEYDOWN:
+                if event.key == SDLK_f:
+                    self.f_pressed=False
+
+            elif event.type == SDL_KEYUP:
+                if event.key == SDLK_f:
+                    self.f_pressed=True
+
 
     def draw(self):
         self.check_image.clip_draw(0,0,check_image_width,check_image_height,self.check_image_x,self.check_image_y,check_image_width*0.4,check_image_height*0.4)
