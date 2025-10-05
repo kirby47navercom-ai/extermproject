70b9b87805b35af4fbc8bcad40a90def8202150c
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-06 03:01:25 +0900

    이제 f키로 열고 닫기 ㄱㄱ

--- gesture_background.py
+++ gesture_background.py
@@ -8,15 +8,28 @@ check_image_height = 216
 
 class GestureBackground:
     def __init__(self):
-        self.check_image = load_image('Canvas\\1.png')
-        self.canvas_image = load_image('Canvas\\2.png')
-        self.check_image_x = 400
-        self.check_image_y = 300
+        self.check_image = load_image('Canvas\\2.png')
+        self.canvas_image = load_image('Canvas\\1.png')
+        self.check_image_x = canvaswidth//2
+        self.check_image_y = canvasheight-(check_image_height*0.2)
+        self.canvas_image_x=canvaswidth//2
+        self.canvas_image_y=canvasheight+canvasheight//2
+
         pass
 
     def update(self, frame_time, events):
+        for event in events:
+
+            if self.canvas_image_y>canvasheight//2:
+                self.check_image_y-=10
+                self.canvas_image_y-=10
+        pass
+
+
+    def handle_event(self, event):
         pass
 
     def draw(self):
-        self.check_image.clip_draw(0,0,check_image_width,check_image_height,self.check_image_x,self.check_image_y,check_image_width*0.7,check_image_height*0.7)
+        self.check_image.clip_draw(0,0,check_image_width,check_image_height,self.check_image_x,self.check_image_y,check_image_width*0.4,check_image_height*0.4)
+        self.canvas_image.draw(self.canvas_image_x,self.canvas_image_y)
         pass
