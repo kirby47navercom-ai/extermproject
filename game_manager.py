0f62dbee9c3b5b5b308ff04179980addaaa1a9e2
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-06 02:43:11 +0900

    일단 f 그림만 배치

--- game_manager.py
+++ game_manager.py
@@ -2,27 +2,31 @@ from pico2d import *
 import ramona
 import background
 import mouse_image
+import gesture_background
 
 def init():
-    global player,stage_background,mouse
-    player = ramona.Ramona()
+    global player,stage_background,mouse,gesture_bg
     stage_background = background.Background('1')
+    gesture_bg = gesture_background.GestureBackground()
+    player = ramona.Ramona()
     mouse = mouse_image.Mouse()
 
     pass
 
 def update(frame_time):
-    global player, stage_background
+    global player,stage_background,mouse,gesture_bg
     events = get_events()
     stage_background.update(frame_time)
+    gesture_bg.update(frame_time,events)
     player.update(frame_time,events)
     mouse.update(frame_time,events)
     pass
 
 def render():
-    global player, stage_background
+    global player,stage_background,mouse,gesture_bg
     clear_canvas()
     stage_background.draw()
+    gesture_bg.draw()
     player.draw()
     mouse.draw()
     update_canvas()
--- gesture_background.py
+++ gesture_background.py
@@ -1,16 +1,22 @@
 from pico2d import *
 from canvas_size import *
 
+check_image_width = 825
+check_image_height = 216
+
+
+
 class GestureBackground:
     def __init__(self):
-        self.image = load_image('Canvas\\1.png')
-        self.x = 400
-        self.y = 300
+        self.check_image = load_image('Canvas\\1.png')
+        self.canvas_image = load_image('Canvas\\2.png')
+        self.check_image_x = 400
+        self.check_image_y = 300
         pass
 
     def update(self, frame_time, events):
         pass
 
     def draw(self):
-        self.image.draw(self.x, self.y)
+        self.check_image.clip_draw(0,0,check_image_width,check_image_height,self.check_image_x,self.check_image_y,check_image_width*0.7,check_image_height*0.7)
         pass
