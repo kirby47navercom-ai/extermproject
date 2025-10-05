85e1862f1418396da820d854b9d1f73a999c001a
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-06 00:54:05 +0900

    키보드 입력을 한번에 받기 위한 event_manager 추가

--- game_manager.py
+++ game_manager.py
@@ -7,7 +7,7 @@ def init():
     global player,stage_background,mouse
     player = ramona.Ramona()
     stage_background = background.Background('1')
-    mouse = mouse_image.Mouse()
+    #mouse = mouse_image.Mouse()
 
     pass
 
@@ -15,7 +15,7 @@ def update(frame_time):
     global player, stage_background
     stage_background.update(frame_time)
     player.update(frame_time)
-    mouse.update(frame_time)
+    #mouse.update(frame_time)
     pass
 
 def render():
@@ -23,6 +23,6 @@ def render():
     clear_canvas()
     stage_background.draw()
     player.draw()
-    mouse.draw()
+    #mouse.draw()
     update_canvas()
     pass
