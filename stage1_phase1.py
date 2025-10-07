db345f668ea247145f2d413777c40f61794f3e19
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-08 03:53:30 +0900

    스테이지 분할로 만들자

---
+++ stage1_phase1.py
@@ -0,0 +1,51 @@
+from pico2d import *
+
+import resource
+from canvas_size import *
+import ramona
+from resource import *
+import random
+import math
+from ghost_normal import *
+import draw_gesture
+
+ghost_phase1_far=50
+ghost_phase1_pos=[-1,-1,1,1,1]
+
+class Stage1_Phase1:
+    def __init__(self):
+        self.phase1=[Ghost() for _ in range(5)]
+        for i in range(self.phase1.__len__()):
+            self.phase1[i].x=0 if ghost_phase1_pos[i] == -1 else canvaswidth+ghost_phase1_pos[i]*ghost_phase1_far
+            self.phase1[i].y=random.randint(0,canvasheight)
+
+
+        pass
+    def update(self, frame_time, events=None):
+        for i in range(self.phase1.__len__()):
+            self.phase1[i].update(frame_time,events)
+
+        self.shape_check()
+        self.monster_die()
+        pass
+
+    def shape_check(self):
+        for i in range(len(self.phase1) - 1, -1, -1):
+            if self.phase1[i].shape.name==draw_gesture.result:
+                self.phase1[i].hp-=ramona.Ramona_attack
+                self.phase1[i].hit_animation=True
+                self.phase1[i].hit_frame = 0
+                canvas_size.start_shake(0.5, 5.0)
+
+        draw_gesture.result=None
+
+        pass
+    def monster_die(self):
+        for i in range(len(self.phase1) - 1, -1, -1):
+            if self.phase1[i].die:
+                self.phase1.pop(i)
+        pass
+    def draw(self):
+        for i in range(self.phase1.__len__()):
+            self.phase1[i].draw()
+        pass
