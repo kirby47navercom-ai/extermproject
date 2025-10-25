4b2771242332d051bc12c45a2ee40322f6ec13b8
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-10-26 03:55:24 +0900

    캐릭터에게 돌진하는 공격 1 만듬

--- hellkitty.py
+++ hellkitty.py
@@ -44,13 +44,15 @@ class Boss_Kitty:
 
         self.attack_start=False
         self.attack1= []
+        self.attack1_effect=[] #현재 위치  크기 프레임
         self.attack_init=False
         self.attack1_time = 0.0
-        self.attack1_timer = 1.0
+        self.attack1_timer = 0.2
         self.attack1_frame=0
-        self.attack1_num=5
-        self.attack1_player_speed=400.0
+        self.attack1_num=8
+        self.attack1_player_speed=1200.0
         self.attack1_speed=40.0
+        self.attack1_effect_speed=8.0
 
         self.idle_frame = 0
         self.animation_speed = 4.0
@@ -74,6 +76,15 @@ class Boss_Kitty:
             elif self.current_pattern == 3:
                 self.pattern2(frame_time)
 
+        if self.attack1_effect.__len__()>0:
+            for i in range(len(self.attack1_effect)-1, -1, -1):
+                self.attack1_effect[i][4]=(self.attack1_effect[i][4] + self.attack1_speed * frame_time) % 28
+                self.attack1_effect[i][2]-=self.attack1_effect_speed*frame_time
+                self.attack1_effect[i][3]-=self.attack1_effect_speed*frame_time
+                if self.attack1_effect[i][2]<=0 or self.attack1_effect[i][3]<=0:
+                    self.attack1_effect.pop(i)
+                pass
+
 
         pass
 
@@ -92,18 +103,28 @@ class Boss_Kitty:
 
     def pattern0(self, frame_time):
         if not self.attack_init:
-            self.attack1.append([self.x, self.y,0,ramona.Ramona_POS_X,ramona.Ramona_POS_Y])#현재 위치, 프레임, 캐릭 위치 점점 늘어남
+            self.attack1.append([self.x, self.y,0,ramona.Ramona_POS_X,ramona.Ramona_POS_Y,0.0])#현재 위치, 프레임, 캐릭 위치 점점 늘어남, 이펙트 나오는 시간
+            canvas_size.start_shake(0.1, 5)
             self.attack_init = True
             pass
         else:
             for i in range(len(self.attack1)-1, -1, -1):
                 self.attack1[i][2]= (self.attack1[i][2] + self.attack1_speed * frame_time) % 28
                 self.attack1[i][0],self.attack1[i][1],self.attack1[i][3],self.attack1[i][4]=canvas_size.distance_funtion2(self.attack1[i][0],self.attack1[i][1],self.attack1[i][3],self.attack1[i][4],frame_time,self.attack1_player_speed,self.attack1[i][3],self.attack1[i][4])
-                if self.attack1[i][0]<-50:
+                self.attack1[i][5]+=frame_time
+
+                if self.attack1[i][5]>self.attack1_timer:
+                    self.attack1_effect.append([self.attack1[i][0],self.attack1[i][1],20,20,0])
+                    self.attack1[i][5]=0
+
+
+                if self.attack1[i][0]<-50 or self.ramonatoattack1(i):
+                    canvas_size.start_shake(0.1, 5)
                     self.attack1_num -=1
                     self.attack1.pop(i)
                     if self.attack1_num==0:
                         break
+                    self.attack1.append([self.x, self.y, 0, ramona.Ramona_POS_X, ramona.Ramona_POS_Y,0.0])
 
                 pass
 
@@ -125,8 +146,20 @@ class Boss_Kitty:
 
     def rereset(self):
         pass
-    def ramonatokitty(self):
-        pass
+    def ramonatoattack1(self,i):
+        ax, ay = self.attack1[i][0], self.attack1[i][1]
+        rx, ry = ramona.Ramona_POS_X, ramona.Ramona_POS_Y
+        threshold = 40  # 충돌 반경: 필요에 따라 조정
+        dx = ax - rx
+        dy = ay - ry
+        b = dx * dx + dy * dy <= threshold * threshold and not ramona.Ramona_invincible and not ramona.Ramona_roll_invincible
+        if b:
+            if ramona.CURRENT_HP>0:
+                ramona.CURRENT_HP-=1
+                ramona.Ramona_invincible=True
+                ramona.invincible_timer=0.0
+                canvas_size.start_shake(0.5, 5.0)
+        return b
 
     def hit_kitty_animation(self, frame_time):
         pass
@@ -147,15 +180,18 @@ class Boss_Kitty:
 
             if self.attack1_num > 0 and self.current_pattern==0 and self.attack1.__len__()>0:
                 for i in self.attack1:
-                    cout(i)
                     ax,ay= boss_kitty_attack_coordinate[int(i[2])][2:4]
-                    Boss_Kitty.attack1_image[int(i[2])].clip_draw(0,0,ax,ay,i[0],i[1],ax*1.5,ay*1.5)
+                    Boss_Kitty.attack1_image[int(i[2])].clip_draw(0,0,ax,ay,i[0]-canvas_size.camera_x,i[1]-canvas_size.camera_y,ax*1.5,ay*1.5)
+            if self.attack1_effect.__len__()>0:
+                for i in self.attack1_effect:
+                    cout(int(i[4]))
+                    ex,ey= boss_kitty_attack_coordinate[int(i[4])][2:4]
+                    Boss_Kitty.attack1_image[int(i[2])].clip_draw(0,0,ex,ey,i[0]-canvas_size.camera_x,i[1]-canvas_size.camera_y,i[2],i[3])
 
 
 
 
-
-            self.image[int(self.idle_frame)].clip_draw(0, 0, bx, by, self.x, self.y, bx * SIZE, by * SIZE)
+            self.image[int(self.idle_frame)].clip_draw(0, 0, bx, by, self.x-canvas_size.camera_x, self.y-canvas_size.camera_y, bx * SIZE, by * SIZE)
 
 
         pass
