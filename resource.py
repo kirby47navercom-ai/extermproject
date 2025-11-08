8bf6d727427e76e3f043616cc47e4aeab798c979
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-11-08 21:12:42 +0900

    이미지 점프 까지 넣음

--- resource.py
+++ resource.py
@@ -236,6 +236,18 @@ boss_kitty_die_image= []
 boss_kitty_uibim_image= []
 
 #스테이지 3리소스
+boss_siho_coordinate = [0,0,64,64,0,0]
+boss_fox_coordinate = [0,0,128,64,0,0]
+
+
+boss_siho_appear_image = []
+
+boss_siho_jump_prepare_image = []
+boss_siho_jump_up_image = []
+boss_siho_jump_down_image = []
+boss_siho_jump_over_image = []
+
+
 
 
 
@@ -297,6 +309,18 @@ def load_resources():
     for i in range(4):
         boss_kitty_die_image.append(load_image(f'2stage\\blast{str(i+1)}.png'))
 
+    for i in range(8):
+        boss_siho_appear_image.append(load_image(f'3stage\\Appear{str(i+1)}.png'))
+    for i in range(3):
+        boss_siho_jump_prepare_image.append(load_image(f'3stage\\Fox Human Jump Prepare{str(i+1)}.png'))
+
+    boss_siho_jump_up_image.append(load_image(f'3stage\\Fox Human Jump Cast a.png'))
+
+    for i in range(4):
+        boss_siho_jump_down_image.append(load_image(f'3stage\\Fox Human Jump Cast b{str(i + 1)}.png'))
+
+    for i in range(5):
+        boss_siho_jump_over_image.append(load_image(f'3stage\\Fox Human Jump Over{str(i + 1)}.png'))
 
     ramona_image = {
         'idle': image_idle, 'walk': image_walk, 'run': image_run, 'jump': image_jump,
